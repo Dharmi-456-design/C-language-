f172414d82be17e9d08f84d3f84880bd7e2b4cc5
Your Name <you@example.com>
2026-03-30 21:46:44 +0530

    add

---
+++ C12.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+int main(){
+  
+// int n;
+// printf("Enter the num:");                  //output 3=012
+// scanf("%d",&n);                            //output 4=0123
+
+// for(int i=n; i>0; i--){
+//     for (int j=0; j<n; j++){
+//         printf("%d",j);
+//     }
+//     printf("\n");
+// }
+// printf("\n");
+
+
+
+// int n;
+// printf("Enter the num:");
+// scanf("%d",&n);
+
+// for(int i=n; i>0; i--){
+//     for (int j=0; j<n; j++){
+//         printf("i:%d j:%d\t",i,j);
+//     }
+//     printf("\n");
+// }
+// printf("\n");
+
+
+// int n;
+// printf("Enter num:") ;                 //output 3
+// scanf("%d",&n);                       // 22
+//                                      // 111
+// for(int i=n; i>0; i--){
+//     for (int j=0; j<n-i+1; j++){
+//         printf("%d",i);
+//     }
+//     printf("\n");
+// }
+// printf("\n");
+
+// int n;
+// printf("Enter the num:");
+// scanf("%d",&n);
+
+// for(int i=n; i>0; i--){
+//     for (int j=1; j<n-i+1; j++){
+//         printf("%d",j);
+//     }
+//     printf("\n");
+// }
+// printf("\n");
+
+// int n;
+// printf("Enter the num:");
+// scanf("%d",&n);
+
+// for(int i=68; i>65; i--){
+//     for (int j=65; j<=i; j++){
+//         printf("%c",j);
+//     }
+//     printf("\n");
+// }
+// printf("\n");
+
+
+// int n;
+// printf("Enter the num:");
+// scanf("%d",&n);
+
+// for(int i=68; i>65; i--){
+//     for (int j=65; j<68-i+66; j++){
+//         printf("%c",j);
+//     }
+//     printf("\n");
+// }
+// printf("\n");
+
+
+// int n;
+// printf("Enter the num:");
+// scanf("%d",&n);
+
+// for(int i=68; i>65; i--){
+//     for (int j=67; j>i-2; j--){
+//         printf("%c",j);
+//     }
+//     printf("\n");
+// }
+// printf("\n");
+ 
+                       // 2nd method
+
+// int n,i,j;
+// scanf("%d",&n);
+// for(i=0; i<n; i++){
+//     for(j=0; j<=i; j++){
+//                 printf("%c",'C'-j);
+//     }
+//     printf("\n");
+// }
+    return 0;
+}
