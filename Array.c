ac4a0055a47835782fd81eaac3b6604c5bc8319a
Your Name <you@example.com>
2026-03-30 21:45:54 +0530

    array.c

---
+++ Array.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+int main(){
+
+    // int arr[] = {1, 2, 2, 3, 4, 4, 4, 5};
+    // int n = sizeof(arr) / sizeof(arr[0]);
+    // int freq[n];
+       
+    // for (int i = 0; i < n; i++) {
+    //     freq[i] = -1;
+    // }
+    // for (int i = 0; i < n; i++) {
+    //     int count = 1;
+    //     for (int j = i + 1; j < n; j++) {
+    //         if (arr[i] == arr[j]) {
+    //             count++;
+    //             freq[j] = 0;
+    //         }
+    //     }
+    //     if (freq[i] != 0) {
+    //         freq[i] = count;
+    //     }
+    // }
+
+    
+    // printf("Element | Frequency\n");
+    // for (int i = 0; i < n; i++) {
+    //     if (freq[i] != 0) {
+    //         printf("   %d    |    %d\n", arr[i], freq[i]);
+    //     }
+    // }
+ 
+// int arr[10]={1,3,8,1,7,8,4,6,3,4};
+// int count=0;
+// int len=sizeof(arr)/sizeof(arr[0]);
+
+// for( int j=0; j<len; j++){
+//     for( int i=0; i<len; i++){
+//  if(arr[j]==arr[i]){
+//      count++;
+//  }
+
+//     }
+// printf("Freq of  %d: %d \n" , arr[j],count);
+// count=0;
+// }
+
+     //QUESTION 1-12
+
+// int n=7;
+// for(int a=1; a<=7; a++){
+//     for(int k=7; k>=a; k--){
+//         printf("*");
+// }
+// printf("\n");
+// }
+
+    //QUESTION 2-12
+
+// int n=7;
+// for(int a=1; a<=7; a++){
+//     for(int k=1; k<=a; k++){
+//         printf("%d",k);
+// }
+// printf("\n");
+// }
+
+    //QUESTION 3-12
+
+    // int arr[6]={2, 4, 3, 5, 6, 1};
+
+// for(int i=0; i<=6; i++){
+//         for(int p=0; p<=i; p++){
+//         if(arr[i]+arr[p]==7){
+// printf("(%d,%d)",arr[i],arr[p]);
+//         }
+//     }
+// }
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+    
